e8356b1ebab6bc5e4496ba0b1bb98aba04c05080
Deepanshu-0ne8 <patiidardeepanshu910@gmmail.com>
2025-10-02 19:52:19 +0530

    created auth controllers

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ import { PORT } from './config/env.js';
 import connectDB from './database/mongodb.js';
 import authRouter from './routes/auth.routes.js';
 import reportRouter from './routes/report.routes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/reports', reportRouter);
---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import { JWT_SECRET } from "../config/env.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const authorize = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "User is not Authenticated"
+            })
+        }
+        const decode = jwt.verify(token, JWT_SECRET);
+        if (!decode) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Token"
+            })
+        }
+
+        
+        const user = await User.findById(decode.userId).select("-password");
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+        req.user = user;
+        req.id = decode.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorized', error: error.message });
+    }
+}
