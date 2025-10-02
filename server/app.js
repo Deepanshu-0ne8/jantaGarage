df1a3631730df2da83d0aedf43b09976c658fe96
Deepanshu-0ne8 <patidardeepanshu910@gmail.com>
2025-10-02 15:57:42 +0530

    database setup

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import connectDB from './database/mongodb.js';
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+    connectDB();
+})
+
+export default app;
