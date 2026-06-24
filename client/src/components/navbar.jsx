d9873e5d0a26eb03d6ec50bad943fd9415276bde
Deepanshu-0ne8 <patidardeepanshu910@gmail.com>
2026-06-24 22:03:48 +0530

    solved import bug

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../context/authContext";
-import { useSocket } from "../context/SocketContext"; 
+import { useSocket } from "../context/socketContext"; 
 import { getNotifications, removeNotification, clearAllNotifications } from "../services/UserServices"; 
 import logo from "../assets/logo.png";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
