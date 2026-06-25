491824c7698201cde2e0361b302cd3f312975aba
Deepanshu-0ne8 <patidardeepanshu910@gmail.com>
2026-06-25 15:17:34 +0530

    smtp working check in production

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@ import authRouter from './routes/auth.routes.js';
 import reportRouter from './routes/report.routes.js';
 import userRouter from './routes/user.routes.js';
 import reportRoutes from "./routes/report.routes.js";
-import { CLIENT_URL } from './config/env.js';
+import { CLIENT_URL, APP_PASS, USER_MAIL } from './config/env.js';
+import nodemailer from "nodemailer";
 
 const app = express();
 app.set("trust proxy", true);
@@ -22,6 +23,30 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: USER_MAIL,
+    pass: APP_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
+
+app.get("/smtp-test", async (req, res) => {
+  try {
+    await transporter.verify();
+    console.log("SMTP server is working");
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
